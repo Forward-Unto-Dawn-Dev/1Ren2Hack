dfa38c64765c9fda5a30cc261e7fb875198f6b07
hyperskII942 <deniska.pikalov@gmail.com>
2024-07-11 00:57:24 +0700

    OMG, how to fix dis freakin GITIGNORE, ehh??

--- 1ren2hack.py
+++ 1ren2hack.py
@@ -4,19 +4,29 @@ from utils.orth_base import *
 
 clear()
 ui = ['Information about novell build', 'Run novell build']
+func_enum = []
+func_names = []
 
 print('Welcome to 1Ren2Hack.')
 print('')
 print('Select what you need:')
 
 for x,i in enumerate(ui):
-    print(f'    [{x+1}] {i}')
+    func_enum.append(x)
+    func_names.append(i)
+    print(f'    [{func_enum[x]}] {i}')
 
 print('')
 result = input()
+func_enum = str(func_enum)
 
-if result == '':
+if result not in func_enum:
     clear()
     exit()
+
+if result == '1':
+    print('Select path to your novell.')
+    dir = getpath()
+    print(dir)
 
 exit()
--- utils/orth_base.py
+++ utils/orth_base.py
@@ -3,18 +3,27 @@ import sys
 from contextlib import contextmanager
 import time
 import ctypes
+from tkinter.filedialog import askdirectory
+from tkinter import *
 
-
+def getpath():
+    "Creates a window for selecting a path. Used in conjunction with a variable to save."
+    window = Tk()
+    window.withdraw()
+    path = askdirectory(parent=window, title='Select path to your novell')
+    return path
 
 def errwait():
     "A simple 1-second pause after outputting an error."
     time.sleep(1)
+
 @contextmanager
 def except_handler(exc_handler):
     "Modified exception output module handler."
     sys.excepthook = exc_handler
     yield
     sys.excepthook = sys.__excepthook__
+
 def err(type, value, traceback):
     "New type of exception."
     print(': '.join([str(type.__name__), str(value)]))
@@ -22,4 +31,6 @@ def err(type, value, traceback):
     errwait()
     print("")
 
-clear = lambda: os.system('cls')
+def clear():
+    "Clears the command line."
+    os.system('cls')
